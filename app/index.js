82d47df7cb8ac7d8a7062d8da81a9c0aaa939ee9
Gabrielle Latreille <gabrielle.latreille@shopify.com>
2018-05-26 00:17:34 -0400

    changes, additions and refactoring

--- app/index.js
+++ app/index.js
@@ -1,11 +1,51 @@
-// NOTE: cp means checkpoint
-let energy = {"count": 0, "cp": 10, "cpBool": false, "interval": 1000};
-let universe = {"size": 0, "cp1":false, "cp2": false, "cp3": false, "cp4": false};
+class Currency {
+  constructor(id, unit, count, cp, increment) {
+    this.id = id; //is based on html id
+    this.unit = unit
+    this.count = count;
+    this.cp = [];
+    for (let i = 0; i < cp; i++) {
+      this.cp[i] = false;
+    }
+    this.increment = increment;
+  }
+
+  increase() {
+    this.count += this.increment;
+    document.getElementById(this.id).innerHTML = `${this.count}${this.unit}`
+  }
+
+  decrease(num) {
+    if (this.count - num >= 0) {
+      this.count -= num;
+      document.getElementById(this.id).innerHTML = `${this.count}${this.unit}`
+      return true;
+    }
+    return false;
+  }
+}
+
+let universe = {
+  "size": 0,
+  "cp1": false,
+  "cp2": false,
+  "cp3": false,
+  "cp4": false
+};
 let atmosphereBool = false;
-let resource = {"count": 0, "increment": 1, "createBool": false, "price": 100};
 let fib = []
-let clickCounter = {"expandUniv": 0, "lanscape": 0, "resource": 0, "human": 0}
+let clickCounter = {
+  "expandUniv": 0,
+  "lanscape": 0,
+  "resource": 0,
+  "human": 0
+}
 const humanCost = 20;
+let energy = new Currency("energyCount", "J", 0, 1, 1);
+let resource = new Currency("resourceCount", "N", 0, 1, 1);
+let resourceCost = 10;
+let humanCount = 0;
+
 
 // NOTE: onload
 // window.onload = () => {
@@ -22,35 +62,21 @@ const humanCost = 20;
 // }
 
 // NOTE: COLLECT ENERGY
-document.getElementById("collectEnergy").addEventListener (
+document.getElementById("collectEnergy").addEventListener(
   'click', () => {
-      increaseEnergy();
+    energy.increase();
+    if (energy.count >= 10 && !energy.cp[0]) {
+      enableElement("createBB");
+      enableElement("createBBLabel");
+      energy.cp[0] = true;
+    }
   }
-);
-
-function increaseEnergy() {
-  energy.count++;
-  document.getElementById('energyCount').innerHTML = energy.count;
-  if(energy.count >= energy.cp && !energy.cpBool){
-    enableElement("createBB");
-    enableElement("createBBLabel");
-    energy.cpBool = !energy.cpBool;
-  }
-}
-
-function decreaseEnergy(num) {
-  if(energy.count - num >= 0){
-    energy.count -= num;
-    document.getElementById('energyCount').innerHTML = energy.count;
-    return true;
-  }
-  return false;
-}
+)
 
 // NOTE: CREATE BIG BANG
-document.getElementById("createBB").addEventListener (
+document.getElementById("createBB").addEventListener(
   'click', () => {
-    if(decreaseEnergy(20)){
+    if (energy.decrease(20)) {
       disableElement("createBB");
       disableElement("createBBLabel");
       enableElement("expandUniverse");
@@ -61,36 +87,36 @@ document.getElementById("createBB").addEventListener (
 );
 
 // NOTE: EXPAND UNIVERSE
-document.getElementById("expandUniverse").addEventListener (
+document.getElementById("expandUniverse").addEventListener(
   'click', () => {
-    if(decreaseEnergy(20+10*clickCounter.expandUniv)){
+    if (energy.decrease(20 + 10 * clickCounter.expandUniv)) {
       clickCounter.expandUniv++;
       universe.size = fibonacci(clickCounter.expandUniv, fib);
-      document.getElementById("expandUniverseCost").innerHTML = `${20+10*clickCounter.expandUniv}g energy`;
-      // TODO: format number to be in format 123,456 or 123 456
+      document.getElementById("expandUniverseCost").innerHTML = `${20+10*clickCounter.expandUniv}J energy`;
+      // IDEA: format number to be in format 123,456 or 123 456
       document.getElementById("universeSize").innerHTML = `${universe.size} km${"2".sup()}`;
-      if(clickCounter.expandUniv == 15 && !universe.cp1){
+      if (clickCounter.expandUniv == 15 && !universe.cp1) {
         enableElement("formAtmosphere");
         enableElement("atmosphereCost");
         universe.cp1 = !universe.cp1;
-      } else if(clickCounter.expandUniv >= 25 && universe.cp1 && !universe.cp2 && atmosphereBool){
+      } else if (clickCounter.expandUniv >= 25 && universe.cp1 && !universe.cp2 && atmosphereBool) {
         enableElement("landscaping");
         enableElement("landscapeCost");
         enableElement("waterPerc");
         universe.cp2 = !universe.cp2;
-      } else if(clickCounter.expandUniv >= 45 && universe.cp1 && universe.cp2 && !universe.cp3 && atmosphereBool){
+      } else if (clickCounter.expandUniv >= 45 && universe.cp1 && universe.cp2 && !universe.cp3 && atmosphereBool) {
         enableElement("addResource");
         enableElement("resourceCount");
         enableElement("resourceCost");
         universe.cp3 = !universe.cp3;
-        resource.createBool = true;
+        resource.cp[0] = true;
       } else if (clickCounter.expandUniv == 60 && universe.cp1 && universe.cp2 && universe.cp3 && !universe.cp4 && atmosphereBool) {
         enableElement("createHuman");
         enableElement("humanCost");
         universe.cp4 = !universe.cp4;
       }
 
-      if(clickCounter.expandUniv == 60){
+      if (clickCounter.expandUniv == 60) {
         document.getElementById("expandUniverse").disabled = true;
         document.getElementById("expandUniverseCost").innerHTML = "MAX SIZE";
       }
@@ -108,41 +134,42 @@ function fibonacci(num, memo) {
 
 
 // NOTE: FORM ATMOSPHERE
-document.getElementById("formAtmosphere").addEventListener (
+document.getElementById("formAtmosphere").addEventListener(
   // TODO: if flood is clicked, check to see if any onther buttons should be activated
   'click', () => {
-    if(decreaseEnergy(200)){
-      if(clickCounter.expandUniv == 60){
+    if (energy.decrease(200)) {
+      if (clickCounter.expandUniv == 60) {
         enableElement("createHuman");
         enableElement("humanCost");
-      } if(clickCounter.expandUniv >= 45){
+      }
+      if (clickCounter.expandUniv >= 45) {
         enableElement("addResource");
         enableElement("resourceCost");
         enableElement("resourceCount");
-        resource.createBool = true;
-      } if(clickCounter.expandUniv >= 25){
+        resource.cp[0] = true;
+      }
+      if (clickCounter.expandUniv >= 25) {
         enableElement("landscaping");
         enableElement("landscapeCost");
         enableElement("waterPerc");
       }
       atmosphereBool = true;
-      document.getElementById('memo').innerHTML = "Atmosphere Created";
+      document.getElementById('memo').innerHTML += `</br>Atmosphere Created`;
       document.getElementById('formAtmosphere').disabled = true;
       disableElement('atmosphereCost');
     }
   }
 );
 
-
 // NOTE: LANSCAPE
-document.getElementById("landscaping").addEventListener (
+document.getElementById("landscaping").addEventListener(
   'click', () => {
-    if(clickCounter.lanscape < 71){
-      if(decreaseEnergy(10*(clickCounter.lanscape+1))) {
+    if (clickCounter.lanscape < 71) {
+      if (energy.decrease(10 * (clickCounter.lanscape + 1))) {
         clickCounter.lanscape++;
-        document.getElementById('landscapeCost').innerHTML = `${10*(clickCounter.lanscape+1)}g energy`;
+        document.getElementById('landscapeCost').innerHTML = `${10*(clickCounter.lanscape+1)}J energy`;
         document.getElementById('waterPerc').innerHTML = `${clickCounter.lanscape}%`;
-        if(clickCounter.lanscape == 71){
+        if (clickCounter.lanscape == 71) {
           disableElement('landscapeCost');
           document.getElementById('landscaping').disabled = true;
         }
@@ -152,35 +179,36 @@ document.getElementById("landscaping").addEventListener (
 );
 
 // NOTE: RESOURCES
-document.getElementById("addResource").addEventListener (
+document.getElementById("addResource").addEventListener(
   'click', () => {
-    if(decreaseEnergy(resource.price)){
+    if (energy.decrease(resourceCost)) {
       clickCounter.resource++;
       resource.increment++;
-      resource.price*=2;
-      document.getElementById('resourceCost').innerHTML = `${resource.price}J energy`
+      resourceCost *= 2;
+      document.getElementById('resourceCost').innerHTML = `${resourceCost}J energy`
     }
   }
 )
 
 // NOTE: HUMANS
-document.getElementById("createHuman").addEventListener (
+document.getElementById("createHuman").addEventListener(
   'click', () => {
-    if(decreaseEnergy(humanCost*clickCounter.human)){
+    if (resource.decrease(humanCost * clickCounter.human)) {
       clickCounter.human++;
-      if(clickCounter.human == 1){
-        document.getElementById('memo').innerHTML = "THE FIRST HUMANS HAVE ARRIVED FROM ANOTHER PLANET AND THEIR SHIP CRASHES AND THEY ARE LEFT WITH NOTHING BUT THEMSLEVES"
+      if (clickCounter.human == 1) {
+        document.getElementById('memo').innerHTML += "</br>THE FIRST HUMANS HAVE ARRIVED FROM ANOTHER PLANET AND THEIR SHIP CRASHES AND THEY ARE LEFT WITH NOTHING BUT THEMSLEVES"
         // TODO: allow harvest resources
       }
+      document.getElementById('humanCost').innerHTML = `${humanCost * clickCounter.human}`
     }
   }
 );
 
 // NOTE: CHEAT
-document.getElementById("cheat").addEventListener (
+document.getElementById("cheat").addEventListener(
   'click', () => {
     energy.count += 1000000;
-    increaseEnergy();
+    energy.increase();
     clickCounter.expandUniv = 14;
   }
 );
@@ -214,10 +242,18 @@ function disableElement(elementID) {
   document.getElementById(elementID).style.display = "none";
 }
 
+// NOTE: 1 second interval
 setInterval(() => {
-  increaseEnergy();
-  if(resource.createBool){
-    resource.count+=resource.increment;
-    document.getElementById('resourceCount').innerHTML = resource.count;
+  energy.increase();
+  if (resource.cp[0]) {
+    resource.increase();
   }
+  if(clickCounter.human>=1){
+    if(Math.floor(Math.random()*2)==1){
+      humanCount++;
+    } else {
+      human--;
+    }
+  }
+    // chance it on whether there are more deaths or births
 }, 1000);
